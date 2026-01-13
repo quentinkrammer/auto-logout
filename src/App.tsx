b5513d6980042b1f266e14c06ef62c45134e7336
BastianO <bastian@opatz.me>
2026-01-13 11:33:44 +0100

    store mock credentials in session

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,41 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [isAuthed, setIsAuthed] = useState(
+    () => window.sessionStorage.getItem("isAuthed") === "true"
+  );
+
+  useEffect(() => {
+    window.addEventListener("storage", function (e) {
+      if (e.storageArea === sessionStorage) {
+        console.log("Session storage changed:", e.key, e.oldValue, e.newValue);
+      }
+    });
+  });
+
+  function onLogin() {
+    window.sessionStorage.setItem("isAuthed", "true");
+    setIsAuthed(true);
+  }
+
+  function onLogout() {
+    window.sessionStorage.setItem("isAuthed", "false");
+    setIsAuthed(false);
+  }
 
   return (
-    <>
-      <div>moin</div>
-    </>
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      {isAuthed ? (
+        <button onClick={onLogout}>Logout</button>
+      ) : (
+        <button onClick={onLogin}>Login</button>
+      )}
+
+      <a target="_blank" href={import.meta.env.BASE_URL}>
+        link to open in new tab
+      </a>
+    </div>
   );
 }
 
